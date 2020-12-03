2085cf166d7d3c6b03d1463e5fa553224a477795
KotoRina <rinaergard@yandex.ru>
2020-12-03 21:14:26 +0300

    add norm gnl about 3 function

--- get_next_line.c
+++ get_next_line.c
@@ -6,44 +6,42 @@
 /*   By: rin <rin@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/01 16:16:09 by rin               #+#    #+#             */
-/*   Updated: 2020/12/03 17:08:47 by rin              ###   ########.fr       */
+/*   Updated: 2020/12/03 21:14:05 by rin              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int		get_next_line(int fd, char **line)
+int					get_next_line(int fd, char **line)
 {
-	char	*buf;
-	int		reader;
-	static char	*result;
+	char			*buf;
+	int				reader;
+	char			*result;
 
 	reader = 1;
-	if (fd == -1 || !line || BUFFER_SIZE <= 0)
+	if (!(buf = (char *)malloc(sizeof(char) * 2)) || fd == -1 || !line
+	|| BUFFER_SIZE <= 0 || !(result = (char *)malloc(sizeof(char) * 2)))
 		return (-1);
-	buf = (char *)malloc(sizeof(char) * 2);
-	result = (char *)malloc(sizeof(char) * 2);
-	*line = NULL;
-	while (reader != 0 && buf[0] != '\n')
+	while (reader != 0)
 	{
 		if ((reader = read(fd, buf, 1)) == -1)
 		{
 			free(buf);
 			return (-1);
 		}
+		if (buf[0] == '\n')
+		{
+			*line = result;
+			free(buf);
+			return (1);
+		}
 		buf[1] = '\0';
-		if (buf[0] != '\n' && buf[0] != '\0')
-			result = my_join(result, buf[0]);
-	}
-	*line = result;
-	if (buf[0] == '\n' || buf[0] == '\0')
-	{
-		free(buf);
-		return(1);
+		result = my_join(result, buf[0]);
 	}
 	return (0);
 }
 
+/*
 int main()
 {
 	int		file;
@@ -53,7 +51,9 @@ int main()
 	while(get_next_line(file, &line))
 	{
 		printf("%s\n", line);
-		free(line);
+		//free(line);
 	}
+	//printf("%s\n", line);
 	return (0);
 }
+*/
