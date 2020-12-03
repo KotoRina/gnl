597bfc780772c81317023f0f160d945ad773778b
KotoRina <rinaergard@yandex.ru>
2020-12-04 02:23:26 +0300

    add

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: rin <rin@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/01 16:16:09 by rin               #+#    #+#             */
-/*   Updated: 2020/12/03 21:20:52 by rin              ###   ########.fr       */
+/*   Updated: 2020/12/04 02:22:29 by rin              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int					get_next_line(int fd, char **line)
 
 	reader = 1;
 	if (!(buf = (char *)malloc(sizeof(char) * 2)) || fd == -1 || !line
-	|| BUFFER_SIZE <= 0 || !(result = (char *)malloc(sizeof(char) * 2)))
+	|| BUFFER_SIZE <= 0)
 		return (-1);
 	while (reader != 0)
 	{
@@ -29,14 +29,31 @@ int					get_next_line(int fd, char **line)
 			free(buf);
 			return (-1);
 		}
-		if (buf[0] == '\n')
+		if (buf[0] == '\n' || reader == 0)
 		{
 			*line = result;
 			free(buf);
-			return (1);
+			return (reader == 0 ? 0 : 1);
 		}
 		buf[1] = '\0';
 		result = my_join(result, buf[0]);
 	}
+	return (0);
+}
+
+
+int main()
+{
+	int		file;
+	char	*line;
+
+	file = open("test", O_RDONLY);
+	while(get_next_line(file, &line))
+	{
+		printf("%s\n", line);
+		free(line);
+	}
+	printf("%s\n", line);
+	free(line);
 	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,16 @@
 /*   By: rin <rin@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/01 16:16:13 by rin               #+#    #+#             */
-/*   Updated: 2020/12/03 21:18:10 by rin              ###   ########.fr       */
+/*   Updated: 2020/12/04 02:22:53 by rin              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_HEXT_LINE_H
+#ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # include <stdlib.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <unistd.h>
 
 char		*my_join(char *line, char c);
 size_t		my_strlen(char	*line);
